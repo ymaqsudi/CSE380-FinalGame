980d543abaf153d5d164174b5a05e2b931fa4291
Zijun Wei <Zijun.wei.1@cs.stonybrook.edu>
2024-04-09 16:42:14 -0400

    about scene updated.

--- src/Homework5/Scenes/about.ts
+++ src/Homework5/Scenes/about.ts
@@ -49,43 +49,62 @@ export default class AboutScene extends MenuScene {
 
     // About text Label
     let text1 = <Label>this.add.uiElement(UIElementType.LABEL, "Main", {
-      position: new Vec2(size.x, size.y - 100),
+      position: new Vec2(size.x, size.y - 120),
       text: "This world has always been various shades of black, gray, and white.",
     });
     text1.textColor = Color.WHITE;
     text1.font = "PixelSimple";
-    text1.fontSize = 22;
+    text1.fontSize = 24;
     text1.setHAlign("center");
     text1.setVAlign("center");
 
-    // About text Label
     let text2 = <Label>this.add.uiElement(UIElementType.LABEL, "Main", {
-      position: new Vec2(size.x, size.y - 75),
-      text: "But you have recently heard stories of these hidden items that once found, can help to light the world up in colors never seen before.",
+      position: new Vec2(size.x, size.y - 80),
+      text: "But you have recently heard stories of these hidden items that once found,",
     });
     text2.textColor = Color.WHITE;
     text2.font = "PixelSimple";
-    text2.fontSize = 22;
+    text2.fontSize = 24;
     text2.setHAlign("center");
     text2.setVAlign("center");
 
+    let text2_0 = <Label>this.add.uiElement(UIElementType.LABEL, "Main", {
+      position: new Vec2(size.x, size.y - 40),
+      text: "can help to light the world up in colors never seen before.",
+    });
+    text2_0.textColor = Color.WHITE;
+    text2_0.font = "PixelSimple";
+    text2_0.fontSize = 24;
+    text2_0.setHAlign("center");
+    text2_0.setVAlign("center");
+
     let text3 = <Label>this.add.uiElement(UIElementType.LABEL, "Main", {
-      position: new Vec2(size.x, size.y - 50),
-      text: "Enthralled with the story, you head out on an adventure, seeking to transform the world into one full of vivid color.",
+      position: new Vec2(size.x, size.y),
+      text: "Enthralled with the story, you head out on an adventure,",
     });
     text3.textColor = Color.WHITE;
     text3.font = "PixelSimple";
-    text3.fontSize = 22;
+    text3.fontSize = 24;
     text3.setHAlign("center");
     text3.setVAlign("center");
 
+    let text3_0 = <Label>this.add.uiElement(UIElementType.LABEL, "Main", {
+      position: new Vec2(size.x, size.y + 40),
+      text: "seeking to transform the world into one full of vivid color.",
+    });
+    text3_0.textColor = Color.WHITE;
+    text3_0.font = "PixelSimple";
+    text3_0.fontSize = 24;
+    text3_0.setHAlign("center");
+    text3_0.setVAlign("center");
+
     let authors = <Label>this.add.uiElement(UIElementType.LABEL, "Main", {
-      position: new Vec2(size.x, size.y + 100),
+      position: new Vec2(size.x, size.y + 160),
       text: "Presented by Sabrinna K, Zijun W, and Yaseen M",
     });
     authors.textColor = Color.WHITE;
     authors.font = "PixelSimple";
-    authors.fontSize = 22;
+    authors.fontSize = 20;
     authors.setHAlign("center");
     authors.setVAlign("center");
   }
